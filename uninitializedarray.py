84c1c06440bd7387bf948941e18882f20925e707
sandra-krishna <144669224+sandra-krishna@users.noreply.github.com>
2024-07-29 11:57:23 +0530

    Add files via upload

---
+++ uninitializedarray.py
@@ -0,0 +1,15 @@
+print("SJC23MCA-2046 : SANDRA KRISHNA P S")
+print("Batch : MCA 2023-25")
+import numpy as np
+# Create an uninitialized array
+uninitialized_array = np.empty(shape=(2, 3))
+print("Uninitialized Array:")
+print(uninitialized_array)
+# Create an array with all elements as 1
+ones_array = np.ones(shape=(2, 3))
+print("Array with All Elements as 1:")
+print(ones_array)
+# Create an array with all elements as 0
+zeros_array = np.zeros(shape=(2, 3))
+print("Array with All Elements as 0:")
+print(zeros_array)

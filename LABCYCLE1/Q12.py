418e02fb59b60da2e9a99fae9456c4e2ca825c24
sandra-krishna <144669224+sandra-krishna@users.noreply.github.com>
2024-08-05 12:33:19 +0530

    Add files via upload

---
+++ LABCYCLE1/Q12.py
@@ -0,0 +1,8 @@
+print("SJC23MCA-2046: SANDRA KRISHNA P S")
+print("Batch : MCA 2023-25")
+import numpy as np;
+X = np.array([[1, 2],
+[3, 4]])
+Y = np.random.rand(*X.shape)
+result = X * 2 + 2 * Y
+print(result)
